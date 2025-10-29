a21d1eb2ceb000d6461b3bace90ec520be8fd775
nicolaj <nicolaj.kragh@icloud.com>
2025-10-29 17:22:14 +0100

    har tilføjet git instrukter

---
+++ test.py
@@ -0,0 +1,11 @@
+#her er en lommeregner
+tal1 = 2
+tal2 = 4
+sum_resultat = tal1 + tal2
+sum2 = tal1 + tal2 + 1
+sum3 = tal1 + tal2 - 1
+print("dig: hvad er", tal1, "+", tal2)
+print("ai: hvorfor spørg du mig. men mit gæt er omkring", sum3, ",", sum_resultat, "eller", sum2)
+
+
+
